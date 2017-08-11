97b9f5a2c3e349dd5797c30b2b7e5f060409ada1
marynganga <marynganga.mw@gmail.com>
2017-08-11 20:50:58 +0300

    Update  madlibs.js content to remove errors

--- js/madlibs.js
+++ js/madlibs.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function() {
+  $("#blanks form").submit(function(event) {
     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
     blanks.forEach(function(blank) {
-      var userInput = $("input." + blank).val();
+      console.log(userInput);
+      var userInput = $("input#" + blank).val();
       $("." + blank).text(userInput).val();
     });
 
-    $("#story").sho();
+    $("#story").show();
+
+    event.preventDefault();
   });
 });
